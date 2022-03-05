86f59f013fecad276fcdb8727214a2a22e698ffb
YeYiChen <xuph@hdu.edu.cn>
2022-03-05 18:27:36 +0800

    加入搜索页面

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="SearchPage"></div>
+  <div class="SearchPage" v-cloak>
+    <van-search v-model="value" placeholder="请输入搜索关键词" show-action @search="onSearch"
+    @cancel="onCancel"/>
+  </div>
 </template>
 
 <script>
 export default {
-mounted(){
-    document.title='搜索頁面'
-}
-}
+  data() {
+    return {
+      value:'',
+    }
+  },
+  mounted() {
+    document.title = "搜索页面";
+  },
+  methods:{
+    onSearch(){},
+    onCancel(){
+      this.value=''
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
